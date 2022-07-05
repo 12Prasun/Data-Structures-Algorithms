721a431cee7143fc70d35da295854510afe3ab01
Prasun Sharma <101198542+12Prasun@users.noreply.github.com>
2022-07-05 17:10:51 +0530

    Time: 32 ms (55.53%), Space: 18.1 MB (36.23%) - LeetHub

---
+++ 135-candy/135-candy.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n=ratings.size();
+        // int l[n],r[n];
+        vector<int>l(n,1);
+        vector<int>r(n,1);
+        int sum=0;
+        
+        /*for(int i=0;i<n;i++)
+        {
+            l[i]=r[i]=1;
+        }*/
+        for(int i=0;i<n-1;i++)
+        {
+            if(ratings[i+1]>ratings[i])
+            {
+                l[i+1]=l[i]+1;
+            }
+        }
+        for(int i=n-2;i>=0;i--)
+        {
+            if(ratings[i]>ratings[i+1])
+            {
+                r[i]=r[i+1]+1;
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+         sum+=max(l[i],r[i]);
+            
+        }
+        return sum;
+        
+        
+                                    
+       
+        
+    }
+    
+    
+};
